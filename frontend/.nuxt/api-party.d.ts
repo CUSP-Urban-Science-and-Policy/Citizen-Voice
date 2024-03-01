00493054438224ef448fe25e1e0365e6e73892ce
Manuel Garcia <gilbert.mg22@gmail.com>
2024-03-01 20:29:37 +0100

    delete front end

--- frontend/.nuxt/api-party.d.ts
+++
@@ -1,4 +0,0 @@
-import type { $Api } from '/Users/mgarciaalvarez/devel/Citizen-Voice/frontend/node_modules/nuxt-api-party/dist/runtime/composables/$api'
-import type { UseApiData } from '/Users/mgarciaalvarez/devel/Citizen-Voice/frontend/node_modules/nuxt-api-party/dist/runtime/composables/useApiData'
-export declare const $cmsApi: $Api
-export declare const useCmsApiData: UseApiData
