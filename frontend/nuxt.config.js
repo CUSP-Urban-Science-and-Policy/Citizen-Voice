df08cb395d754aeeaeb5da83626a7a1045c0b531
esthetech <esthetech@riseup.net>
2025-03-31 13:35:12 +0200

    config

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -1,151 +1,174 @@
-import path from "path"
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 import vuetify from "vite-plugin-vuetify";
 
-const ONE_DAY = 60 * 60 * 24 * 1000
-const ONE_WEEK = ONE_DAY * 7
+const ONE_DAY = 60 * 60 * 24 * 1000;
+const ONE_WEEK = ONE_DAY * 7;
 
+// eslint-disable-next-line no-undef
 export default defineNuxtConfig({
-  ssr: true,
+    modules: [// We are using @pinia/nuxt for the store
+        "@pinia/nuxt", // Nuxt api party is used for the proxy API
+        "nuxt-api-party", // We are not using this now but maybe in the future
+        // '@kevinmarrec/nuxt-pwa',
 
-  build: {
-      transpile: ["vuetify", "vue-toastification/nuxt"],
-  },
+        // We are using tailwind utility classes instead of the Vuetify utility css classes
+        // More info: https://tailwindcss.nuxtjs.org/
+        "@nuxtjs/tailwindcss", // Tailwind
+        "@tailwindcss/postcss", // We are using NuxtOpenFetch for OpenAPI clients
+        // More info: https://nuxt-open-fetch.vercel.app/setup/quick-start
 
-  // For speeding up load time in development, loading in all components from external packages increases the loading time, here we disabling to auto imports
-  components: {
-      dirs: [
-          '~/components'
-      ]
-  },
+        // this adds the vuetify vite plugin
+        // also produces type errors in the current beta release
+        async (options, nuxt) => {
+            nuxt.hooks.hook("vite:extendConfig", (config) => config.plugins.push(vuetify()));
+        },
 
-  experimental: {
-      inlineSSRStyles: false
-  },
+        // Eslint
+        "@nuxt/eslint", "@nuxtjs/leaflet"],
 
-  nitro: {
-      compressPublicAssets: true,
-  },
+    plugins: [
+        //     { src: '@/plugins/vuedraggable' }
+        // { src: '@/plugins/vue-draggable/index.js', ssr: false }
+    ],
+    ssr: true,
 
-  app: {
-      head: {
-          title: "Citizen Voice",
-          link: [
-              { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
-              {
-                  rel: 'stylesheet', href: 'https://unpkg.com/leaflet@1.9.3/dist/leaflet.css',
-                  integrity: 'sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=', crossorigin: ''
-              },
-          ],
-          script: [
-              {
-                  src: "https://unpkg.com/leaflet@1.9.3/dist/leaflet.js",
-                  integrity: "sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=",
-                  crossorigin: ""
-              }
-          ]
-      }
-  },
+    // For speeding up load time in development, loading in all components from external packages increases the loading time, here we disabling to auto imports
+    components: {
+        dirs: ["~/components"],
+    },
 
-  css: [
-      '@mdi/font/css/materialdesignicons.min.css',
-  ],
+    imports: {
+        dirs: [
+            "stores",
+            // Scan top-level modules
+            "composables",
+            // ... or scan modules nested one level deep with a specific name and file extension
+            "composables/*/index.{ts,js,mjs,mts}",
+            // ... or scan all modules within given directory
+            "composables/**",
+        ],
+    },
 
-  imports: {
-      dirs: [
-          'stores',
-          // Scan top-level modules      
-          'composables',
-          // ... or scan modules nested one level deep with a specific name and file extension      
-          'composables/*/index.{ts,js,mjs,mts}',
-          // ... or scan all modules within given directory      
-          'composables/**'
-      ]
-  },
+    app: {
+        head: {
+            title: "Citizen Voice",
+            link: [
+                { rel: "icon", type: "image/x-icon", href: "/favicon.png" },
+                {
+                    rel: "stylesheet",
+                    href: "https://unpkg.com/leaflet@1.9.3/dist/leaflet.css",
+                    integrity: "sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=",
+                    crossorigin: "",
+                },
+            ],
+            script: [
+                {
+                    src: "https://unpkg.com/leaflet@1.9.3/dist/leaflet.js",
+                    integrity: "sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=",
+                    crossorigin: "",
+                },
+            ],
+        },
+    },
 
-  plugins: [
-      //     { src: '@/plugins/vuedraggable' }
-      // { src: '@/plugins/vue-draggable/index.js', ssr: false }
-  ],
+    css: [
+        "@mdi/font/css/materialdesignicons.min.css",
+        "./assets/css/main.css"
+    ],
 
-  vite: {
-      define: {
-          'process.env.DEBUG': false,
-      },
-      ssr: {
-          noExternal: ["vuetify"],
-      },
-  },
+    runtimeConfig: {
+        cookieName: process.env.COOKIE_NAME || "__session",
+        cookieSecret: process.env.COOKIE_SECRET || "secret",
+        cookieExpires: parseInt(process.env.COOKIE_REMEMBER_ME_EXPIRES || ONE_DAY.toString(), 10), // 1 day
+        cookieRememberMeExpires: parseInt(process.env.COOKIE_REMEMBER_ME_EXPIRES || ONE_WEEK.toString(), 10), // 7 days
+        public: {
+            baseAPI: process.env.BASE_API || "http://localhost:3000",
+            apiHostPayment: "",
+        },
+        paymentSecretKey: "",
+    },
 
-  modules: [
-      // We are using @pinia/nuxt for the store
-      '@pinia/nuxt',
+    build: {
+        transpile: ["vuetify", "vue-toastification/nuxt"],
+    },
 
-      // Nuxt api party is used for the proxy API
-      'nuxt-api-party',
+    routeRules: {
+        // Render these routes with SPA // See: https://nuxt.com/docs/guide/concepts/rendering#route-rules
+        "/design/**": { ssr: false },
+        "/design/surveys/**": { ssr: false },
+        "/user/**": { ssr: false },
+        "/survey/**": { ssr: false },
+    },
 
-      // We are not using this now but maybe in the future
-      // '@kevinmarrec/nuxt-pwa',
+    experimental: {
+        inlineSSRStyles: false,
+    },
 
-      // We are using tailwind utility classes instead of the Vuetify utility css classes
-      // More info: https://tailwindcss.nuxtjs.org/
-      '@nuxtjs/tailwindcss',
+    compatibilityDate: "2024-09-04",
 
-      // We are using NuxtOpenFetch for OpenAPI clients
-      // More info: https://nuxt-open-fetch.vercel.app/setup/quick-start
- 
+    nitro: {
+        compressPublicAssets: true,
+    },
 
-      // this adds the vuetify vite plugin
-      // also produces type errors in the current beta release
-      async (options, nuxt) => {
-          nuxt.hooks.hook("vite:extendConfig", (config) => config.plugins.push(vuetify()));
-      },
-  ],
+    vite: {
+        define: {
+            "process.env.DEBUG": false,
+        },
+        ssr: {
+            noExternal: ["vuetify"],
+        },
+    },
 
-  runtimeConfig: {
-      cookieName: process.env.COOKIE_NAME || '__session',
-      cookieSecret: process.env.COOKIE_SECRET || 'secret',
-      cookieExpires: parseInt(process.env.COOKIE_REMEMBER_ME_EXPIRES || ONE_DAY.toString(), 10), // 1 day
-      cookieRememberMeExpires: parseInt(process.env.COOKIE_REMEMBER_ME_EXPIRES || ONE_WEEK.toString(), 10), // 7 days
-      public: {
-          baseAPI: process.env.BASE_API || 'http://localhost:3000',
-          apiHostPayment: '',
-      },
-      paymentSecretKey: '',
-  },
+    typescript: {
+        typeCheck: true,
+    },
 
-  // See: https://github.com/johannschopplich/nuxt-api-party
-  apiParty: {
-      endpoints: {
-          cmsApi: { // Becomes `$cmsApi()` and useCmsApiData()
-              url: process.env.API_PARTY_CMS_URL,
-              schema: './openapi/citizenvoice/openapi.yaml'
-          },
-          authApi: { // Becomes `$authApi()` and useAuthApiData()
-              url: process.env.API_PARTY_AUTH_URL,
-              schema: './openapi/citizenvoice/openapi.yaml'
-          }
-      }
-  },
+    hooks: {
+        //   'pages:extend'(routes) {
+        //       routes.push({
+        //           name: "survey-design",
+        //           path: "/design/surveys/create",
+        //           file: path.resolve(__dirname, './pages/design/surveys/[_id]/index.vue')
+        //       });
+        //       return routes
+        //   }
+    },
 
-  hooks: {
-    //   'pages:extend'(routes) {
-    //       routes.push({
-    //           name: "survey-design",
-    //           path: "/design/surveys/create",
-    //           file: path.resolve(__dirname, './pages/design/surveys/[_id]/index.vue')
-    //       });
-    //       return routes
-    //   }
-  },
+    // See: https://github.com/johannschopplich/nuxt-api-party
+    apiParty: {
+        endpoints: {
+            cmsApi: {
+                // Becomes `$cmsApi()` and useCmsApiData()
+                url: process.env.API_PARTY_CMS_URL,
+                schema: "http://localhost:8000/api/v2/schema",
+            },
+            cmsApiV1: {
+                // Becomes `$cmsApi()` and useCmsApiData()
+                url: process.env.API_PARTY_CMS_URL,
+                schema: "http://localhost:8000/civilian/v1/schema",
+            },
+            // authApi: { // Becomes `$authApi()` and useAuthApiData()
+            //     url: process.env.API_PARTY_AUTH_URL,
+            //     schema: './openapi/citizenvoice/openapi.yaml'
+            // }
+        },
+    },
 
-  routeRules: {
-      // Render these routes with SPA // See: https://nuxt.com/docs/guide/concepts/rendering#route-rules
-      '/design/**': { ssr: false },
-      '/design/surveys/**': { ssr: false },
-      '/user/**': { ssr: false },
-      '/survey/**': { ssr: false },
-  },
-
-  compatibilityDate: '2024-09-04',
-})
+    eslint: {
+        config: {
+            stylistic: {
+                // tabWidth: 4,
+                // useTabs: false,
+                // semi: true,
+                // printWidth: 120, // Adjust this value based on your needs
+                // htmlWhitespaceSensitivity: "ignore",
+                // trailingComma: "es5",
+                // endOfLine: "lf",
+                // arrowParens: "always",
+                // bracketSpacing: true,
+                // embeddedLanguageFormatting: "auto",
+                // vueIndentScriptAndStyle: true,
+            },
+        },
+    },
+});
