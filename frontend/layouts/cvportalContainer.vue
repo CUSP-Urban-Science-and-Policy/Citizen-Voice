c1c3c59814d9285ec8358eb1c7dafe2f5a43ece9
esthetech <esthetech@riseup.net>
2025-03-31 13:36:19 +0200

    layouts

---
+++ frontend/layouts/cvportalContainer.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <HeaderCVPortal />
+    <v-main style="padding-top: 0">
+      <v-container>
+        <div class="pt-10">
+          <slot />
+        </div>
+      </v-container>
+    </v-main>
+    <FooterCVPortal />
+  </v-app>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.custom-zero-padding {
+  padding: 0;
+  padding-left: 0;
+}
+</style>
