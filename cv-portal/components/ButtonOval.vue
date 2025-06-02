1fb8eae81321b91ae68de5e18e1e1acdd02ec6f6
esthetech <esthetech@riseup.net>
2025-06-02 16:21:10 +0200

    adds componets

---
+++ cv-portal/components/ButtonOval.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class=" justify-center items-center inline-flex cursor-pointer">
+        <a :href="href"
+            class="text-center text-white px-4 py-2 bg-black rounded-3xl cursor-pointer text-xl font-semibold font-['Chillax'] leading-tight tracking-wide">{{
+                label }}
+        </a>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    label: String | undefined,
+    href: String | undefined,
+})
+</script>
