f7a3c9745e7b0ad2ec96e2624f1fe3dbdf693fa4
esthetech <esthetech@riseup.net>
2025-06-02 16:40:29 +0200

    Fix: unused props

--- cv-portal/components/ButtonOval.vue
+++ cv-portal/components/ButtonOval.vue
@@ -1,15 +1,15 @@
 <template>
     <div class=" justify-center items-center inline-flex cursor-pointer">
-        <a :href="href"
+        <a :href="props.href"
             class="text-center text-white px-4 py-2 bg-black rounded-3xl cursor-pointer text-xl font-semibold font-['Chillax'] leading-tight tracking-wide">{{
-                label }}
+                props.label }}
         </a>
     </div>
 </template>
 
 <script setup>
 const props = defineProps({
-    label: String | undefined,
-    href: String | undefined,
+    label: String,
+    href: String,
 })
 </script>
