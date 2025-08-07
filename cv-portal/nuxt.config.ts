18e240ebd0013f963de0f83d66c799638af57e05
Manuel Garcia <gilbert.mg22@gmail.com>
2025-08-07 21:55:17 +0200

    fix  error 503

--- cv-portal/nuxt.config.ts
+++ cv-portal/nuxt.config.ts
@@ -35,18 +35,18 @@ export default defineNuxtConfig({
     },
 
     runtimeConfig: {
-        public: {
-            apiPartyEndpointsCmsApiUrl: process.env.NUXT_API_PARTY_ENDPOINTS_CMS_API_URL || 'http://localhost:8000/civilian/v1/'
-        }
-    },
 
-    // API party configuration
-    apiParty: {
-        endpoints: {
-            cmsApiV1: {
-                url: process.env.NUXT_API_PARTY_ENDPOINTS_CMS_API_URL || 'http://localhost:8000/civilian/v1/',
-                schema: `${process.env.NUXT_API_PARTY_ENDPOINTS_CMS_API_URL || 'http://localhost:8000/civilian/v1/'}/schema/`,
-            },
-        }
-    },
+        // API party configuration
+        apiParty: {
+            endpoints: {
+                cmsApiV1: { // Becomes `$cmsApi()` and useCmsApiData()
+                    url: process.env.API_PARTY_CMS_URL || 'http://localhost:8000/voice/v3',
+                    // token: '',
+                    schema: `${process.env.API_PARTY_CMS_URL || 'http://localhost:8000/voice/v3'}/schema`,
+                    // allowedUrls: [ ],
+                }
+            }
+        },
+
+    }
 })
